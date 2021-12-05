f67436ad3cf31f9e4336dafefa1c594a3cc1a321
Edgar Guerrero <edgarguerrero37@gmail.com>
2021-12-05 09:31:44 -0500

    Instalacion de boostrap y modificacion de app.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+
+} from "react-router-dom";
+
+import Register from './Sales/pages/Register';
+import Sales from './Sales/pages/Sales';
+import Products from "./productsManagement/pages/gestionProductos.jsx"
+import Rol from './Roles/pages/Rol';
+import PrivateLayout from './RegistroProductos/layouts/PrivateLayout';
+import Productos from "./RegistroProductos/RegistroyEdicion";
+import Login from './Login/Login';
+import PrivateRoute from "./Shared/components/PrivateRoute";
+import PrivateRouteAdmin from "./Shared/components/PrivateRouteAdmin";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <PrivateRouteAdmin path="/rol" exact component={Rol}></PrivateRouteAdmin>
+        <Route exact path="/" element={ <Home /> } />
+      </Routes>
+    </Router>
   );
 }
 
